cbae49de5e1d178bdbc7131fe74952a893ac2479
Jiangxuelin1205 <Jiangxuelin1205@163.com>
2019-06-19 17:31:01 +0800

    ShopCategoryDao except insert done and tested

--- src/test/java/com/o2o/dao/ShopCategoryDaoTest.java
+++ src/test/java/com/o2o/dao/ShopCategoryDaoTest.java
@@ -1,12 +1,15 @@
 package com.o2o.dao;
 
 import com.o2o.BaseTest;
+import com.o2o.entity.ProductCategory;
 import com.o2o.entity.ShopCategory;
 import org.codehaus.jackson.map.Serializers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ShopCategoryDaoTest extends BaseTest {
@@ -28,5 +31,40 @@ public class ShopCategoryDaoTest extends BaseTest {
         testCategory.setParent(parentCategory);
         List<ShopCategory> shopCategoryList=shopCategoryDao.queryShopCategory(testCategory);
         Assert.assertEquals(1,shopCategoryList.size());
+    }
+
+    @Test
+    public void test_query_shop_category_by_id(){
+        ShopCategory shopCategory=shopCategoryDao.queryShopCategoryById(1L);
+    }
+
+    @Test
+    public void test_query_shop_category_by_ids(){
+        List<Long> ids=new ArrayList<>();
+        ids.add(1L);
+        ids.add(2L);
+        List<ShopCategory> shopCategories=shopCategoryDao.queryShopCategoryByIds(ids);
+        System.out.println(shopCategories.size());
+    }
+
+    @Test
+    public void insert_shop_category(){
+       ShopCategory shopCategory=new ShopCategory();
+       shopCategory.setParent(null);
+       shopCategory.setCreateTime(new Date());
+       shopCategory.setShopCategoryImg("sdf");
+        int effectedNumber=shopCategoryDao.insertShopCategory(shopCategory);
+    }
+
+    @Test
+    public void delete_shop_category(){
+        int effectedNumber=shopCategoryDao.deleteShopCategory(2L);
+    }
+
+    @Test
+    public void batchDelete(){
+        List<Long> ids=new ArrayList<>();
+        ids.add(3L);
+        int effectedNumber=shopCategoryDao.batchDeleteShopCategory(ids);
     }
 }
