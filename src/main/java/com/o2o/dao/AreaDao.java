0118ed696471ae9ffa3ee43c5424724000d1e9c8
Jiangxuelin1205 <Jiangxuelin1205@163.com>
2019-06-19 11:52:16 +0800

    areaDao done and tested

--- src/main/java/com/o2o/dao/AreaDao.java
+++ src/main/java/com/o2o/dao/AreaDao.java
@@ -6,6 +6,27 @@ import java.util.List;
 
 
 public interface AreaDao {
-    //返回列出区域列表
-     List<Area> queryArea();
+
+    /**
+     * 返回列出区域列表
+     */
+    List<Area> queryArea();
+
+
+    int insertArea(Area area);
+
+    /**
+     * 更新区域信息
+     */
+    int updateArea(Area area);
+
+    /**
+     * 删除区域信息
+     */
+    int deleteArea(long areaId);
+
+    /**
+     * 批量删除区域列表
+     */
+    int batchDeleteArea(List<Long> areaIdList);
 }
--- src/test/java/com/o2o/dao/AreaDaoTest.java
+++ src/test/java/com/o2o/dao/AreaDaoTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -14,8 +16,42 @@ public class AreaDaoTest extends BaseTest {
     AreaDao areaDao;
 
     @Test
-    public void testQueryArea(){
-        List<Area> areaList=areaDao.queryArea();
-        Assert.assertEquals(2,areaList.size());
+    public void query_area() {
+        List<Area> areaList = areaDao.queryArea();
+        Assert.assertEquals(2, areaList.size());
+    }
+
+    @Test
+    public void insert_area() {
+        Area area = new Area();
+        area.setAreaName("asdfa");
+        area.setPriority(2);
+        area.setCreateTime(new Date());
+        area.setLastEditTime(new Date());
+        Assert.assertEquals(1, areaDao.insertArea(area));
+    }
+
+    @Test
+    public void update_area() {
+        Area area = new Area();
+        area.setAreaId(3);
+        area.setAreaName("asddfsdfa");
+        area.setPriority(2);
+        area.setCreateTime(new Date());
+        area.setLastEditTime(new Date());
+        Assert.assertEquals(1, areaDao.updateArea(area));
+    }
+
+    @Test
+    public void delete() {
+        Assert.assertEquals(1, areaDao.deleteArea(3L));
+    }
+
+    @Test
+    public void batchDelete() {
+        List<Long> areaIds = new ArrayList<>();
+        areaIds.add(3L);
+        areaIds.add(4L);
+        Assert.assertEquals(2, areaDao.batchDeleteArea(areaIds));
     }
 }
